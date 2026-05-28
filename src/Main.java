41ef9fc7132ceb3e66986ecb0038f83a0712a7b3
anuj kumar <anujkumar13390@gmail.com>
2026-05-28 17:30:07 +0530

    add hello message

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
-import java.lang.reflect.Array;
+
 import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+
 
 public class Main {
     public static void main(String[] args) {
